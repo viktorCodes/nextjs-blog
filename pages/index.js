a4be51cb48563b0d2549e171832e61252f4fbfb8
calimendoza <mbaivictor1@gmail.com>
2023-01-08 21:59:03 -0800

    nextjs-blog app

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link';
+import Date from '../components/date';
 
 export default function Home({ allPostsData }) {
   return (
@@ -10,19 +12,19 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>As you already know, I'm  Viktor. I'm professional, loyal and conscientous and having studied the jod description for this role in detail, I feel the attributes I possess are a strong match. While at work, Im someone who who takes pride in deliverying each task to a high standard and I achieve this by working closely with othe members of my team. I also make sure to get a full brief from my manager or supervisor and team mates as to what they expect me achieve while working in the role im employed in.If successfull at interview I look forward to learning the role quickly and also get to know as much as your product and services so i can start contributing to your business goals and becaome a valuable member of the organisation.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+           <li className={utilStyles.listItem} key={id}>
+           <Link href={`/posts/${id}`}>{title}</Link>
+           <br />
+           <small className={utilStyles.lightText}>
+             <Date dateString={date} />
+           </small>
+         </li>
           ))}
         </ul>
       </section>
