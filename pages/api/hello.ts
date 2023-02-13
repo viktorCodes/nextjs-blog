d6c6f104811a5b745604a558914cf887f5461705
calimendoza <mbaivictor1@gmail.com>
2023-02-13 00:31:56 -0800

    changed blog to typescript

---
+++ pages/api/hello.ts
@@ -0,0 +1,10 @@
+// export default function handler(req, res) {
+//     res.status(200).json({ text: 'Hello' });
+//   }
+
+
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default (_: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ text: 'Hello' })
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,42 @@
+
+
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link';
-import Date from '../components/date';
+import Link from 'next/link'
+import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <p>VIKTOR THE DEV</p>
         <p>As you already know, I'm  Viktor. I'm professional, loyal and conscientous and having studied the jod description for this role in detail, I feel the attributes I possess are a strong match. While at work, Im someone who who takes pride in deliverying each task to a high standard and I achieve this by working closely with othe members of my team. I also make sure to get a full brief from my manager or supervisor and team mates as to what they expect me achieve while working in the role im employed in.If successfull at interview I look forward to learning the role quickly and also get to know as much as your product and services so i can start contributing to your business goals and becaome a valuable member of the organisation.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-           <li className={utilStyles.listItem} key={id}>
-           <Link href={`/posts/${id}`}>{title}</Link>
-           <br />
-           <small className={utilStyles.lightText}>
-             <Date dateString={date} />
-           </small>
-         </li>
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
@@ -32,7 +44,7 @@ export default function Home({ allPostsData }) {
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
